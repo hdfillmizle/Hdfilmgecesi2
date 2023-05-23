fb499fb9c2bc0445d7ee29f2713caaf3dd8f4fac
hdfillmizle <119707352+hdfillmizle@users.noreply.github.com>
2023-05-24 00:17:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let sayac = 1;
+let yarak = 1;
+let dasak = 1;
+let malak = 1;
+let kasar = 1;
+let tas = 1;
+
+while(sayac<=10){
+    console.log(sayac);
+}
+while(yarak<=10){
+    console.log(sayac);
+}
+while(dasak<=10){
+    console.log(sayac);
+}
+while(malak<=10){
+    console.log(sayac);
+}
+while(kasar<=10){
+    console.log(sayac);
+}
+while(tas<=10){
+    console.log(sayac);
+}
